726067048f2e8f8fe183806e0124430a8111e253
Waledahmed <Waledahmed416@gmail.com>
2025-05-13 04:06:30 +0200

    gg

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,19 +1,23 @@
+// android/build.gradle (Kotlin DSL)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Plugin for Android Gradle
+        classpath("com.android.tools.build:gradle:8.2.1")
+
+        // Plugin for Firebase
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
+
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
-}
-
-val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
